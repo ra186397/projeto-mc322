768108154e28e06bc87e6f099962e7d73831b775
Gabriel Vieira <gabrielvieirasousa2001@gmail.com.br>
2021-05-22 15:39:21 -0300

    Vladimir e Leblanc

--- LegendsOfRuneterra/src/card/champion/Champion.java
+++ LegendsOfRuneterra/src/card/champion/Champion.java
@@ -1,8 +1,16 @@
 package card.champion;
 
+import card.Effect;
 import card.Follower;
+import card.Region;
+import card.Trait;
 
 public abstract class Champion extends Follower {
+
+  public Champion(String name, String description, int cost, int baseHealth, int basePower, Region region,
+      Effect[] newEffects, Trait[] traits) {
+    super(name, description, cost, baseHealth, basePower, region, newEffects, traits);
+  }
 
   public abstract void evolve();
 
--- LegendsOfRuneterra/src/card/champion/noxus/Darius.java
+++ LegendsOfRuneterra/src/card/champion/noxus/Darius.java
@@ -2,14 +2,16 @@ package card.champion.noxus;
 
 import card.Effect;
 import card.Region;
+import card.Trait;
 import card.champion.Champion;
 
 public class Darius extends Champion {
 
   private int nexusLife;
 
-  public Darius(String name, String description, int cost, Region region, Effect[] efeitosNovos) {
-
+  public Darius(String name, String description, int cost, int baseHealth, int basePower, Region region,
+      Effect[] newEffects, Trait[] traits) {
+    super(name, description, cost, baseHealth, basePower, region, newEffects, traits);
   }
 
   @Override
