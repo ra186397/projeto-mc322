68d7c82a601ce212da0197e59ab43888a8c2c82a
Giovani <g186397@dac.unicamp.br>
2021-05-22 14:52:59 -0300

    estrutura base do follower feita.

--- LegendsOfRuneterra/src/card/Card.java
+++ LegendsOfRuneterra/src/card/Card.java
@@ -8,17 +8,17 @@ public abstract class Card {
     protected String name;
     protected String description;
     protected int cost;
-    protected Region regiao;
-    protected ArrayList<Effect> efeitos;
+    protected Region region;
+    protected ArrayList<Effect> effects;
 
-    public Card(String name, String description, int cost, Region regiao, Effect[] efeitosNovos) {
+    public Card(String name, String description, int cost, Region region, Effect[] newEffects) {
         this.name = name;
         this.description = description;
         this.cost = cost;
-        this.regiao = regiao;
-        this.efeitos = new ArrayList<Effect>();
-        for (int i = 0; i < efeitosNovos.length; i++) {
-            this.efeitos.add(efeitosNovos[i]);
+        this.region = region;
+        this.effects = new ArrayList<Effect>();
+        for (int i = 0; i < newEffects.length; i++) {
+            this.effects.add(newEffects[i]);
         }
     }
 
