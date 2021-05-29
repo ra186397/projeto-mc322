8ff3ffbef48c3d55c3151e0730d57000f7431c2d
Giovani <g186397@dac.unicamp.br>
2021-05-29 17:23:31 -0300

    Fiz mais um pouco do menu

--- LegendsOfRuneterra/src/menu/Menu.java
+++ LegendsOfRuneterra/src/menu/Menu.java
@@ -3,12 +3,17 @@ package menu;
 import java.lang.System;
 import java.util.Scanner;
 
+import game.Deck;
+
+import java.util.ArrayList;
+
 public class Menu {
 
     private static Menu menu;
+    private ArrayList<Deck> decks;
 
     private Menu() {
-
+        this.decks = new ArrayList<Deck>();
     }
 
     public static Menu getMenu() {
@@ -20,13 +25,22 @@ public class Menu {
 
     public void abrirMenu() {
 
-        int opcao;
+        int option;
+        boolean hasDeck = false;
+        Deck chosenDeck;
         Scanner sc = new Scanner(System.in);
         System.out.println("Bem vindo a Legends Of Runeterra!");
         System.out.println("Para começar a jogar, você deve escolher o seu deck.");
         System.out.println("Você possui os seguintes decks: Demacia"); //alterar
         System.out.println("Digite o número do deck que você deseja usar, ou digite 0 para criar um novo deck.");
-        
+        option = sc.nextInt();
+        if (option == 0) {
+            System.out.println("Depois nois cria o deck, só escolhe o de demacia ai"); //alterar
+        }
+        else if(option >= 1 && option < decks.size()) {
+            System.out.print
+        }
+
         
     
         Game game = Game.getGame();
