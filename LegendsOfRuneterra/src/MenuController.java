524760c1e334dce43e15c748bdc05ddb015b16ac
Gabriel Vieira <gabrielvieirasousa2001@gmail.com.br>
2021-06-25 19:29:50 -0300

    menu

---
+++ LegendsOfRuneterra/src/MenuController.java
@@ -0,0 +1,32 @@
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MenuController {
+
+  @FXML
+  private JFXButton jxj_button;
+
+  @FXML
+  void handleMoveToDeckSelection(ActionEvent event) {
+
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/DeckSelection.fxml"));
+      Parent root = (Parent) fxmlLoader.load();
+      Scene scene = new Scene(root, 1280, 720);
+      Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      stage.setScene(scene);
+      stage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
--- LegendsOfRuneterra/src/runner/Runner.java
+++ LegendsOfRuneterra/src/runner/Runner.java
@@ -1,13 +1,30 @@
 package runner;
 
-import menu.Menu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-
-public class Runner {
+public class Runner extends Application {
     public static void main(String[] args) {
-        
-        Menu menu = Menu.getMenu();
-        menu.openMenu();
+
+        launch(args);
 
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Menu.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1280, 720);
+
+        primaryStage.setMinWidth(1280); // s ets stage width
+        primaryStage.setMinHeight(720); // sets stage heigh
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
