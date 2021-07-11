ee3748a4c3582427006090bc0b0098138c417805
Giovani <g186397@dac.unicamp.br>
2021-07-11 12:12:29 -0300

    Comentários nos traits.

--- LegendsOfRuneterra/src/card/Trait.java
+++ LegendsOfRuneterra/src/card/Trait.java
@@ -2,13 +2,13 @@ package card;
 
 public enum Trait {
     
-    DOUBLE_ATTACK,
-    ELUSIVE,
-    FURY,
-    REGENERATION,
-    BARRIER,
-    QUICK_ATTACK,
-    TOUGH;
+    DOUBLE_ATTACK, //Ataca duas vezes, uma antes e uma ao mesmo tempo do seu bloqueador. Bate duas vezes no nexus se não for bloqueado. (Não funciona na defesa).
+    ELUSIVE, //Só pode ser bloqueada por unidades elusivas.
+    FURY, //Ganha +n/+m toda vez que abate uma unidade.
+    REGENERATION, //Recupera toda a vida perdida no final da rodada.
+    BARRIER, //Anula completamente o primeiro dano que toma. Dura uma rodada.
+    QUICK_ATTACK, //Ataca antes de seu bloqueador (Não funciona na defesa).
+    TOUGH; //Sofre 1 de dano a menos toda vez que sofre dano.
 
 }
 
